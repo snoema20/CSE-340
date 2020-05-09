279f5d6d5619105de57010b57cbaac09087d740b
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-05-09 12:20:03 -0700

    commit

---
+++ Enhancement 1/Home.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="x-ua-compatible" content="ie=edge">
+ <title>Enhancement 1 | Web Backend Development I</title>
+ <meta name="description" content="CSE 340 Enhancement 1 ">
+ <meta name="viewport" content="width=device-width">
+ <link rel="stylesheet" href="#" media="screen">
+</head>
+
+<body>
+ <div id="wrapper">
+  <header id="header"><img src="/#" alt="Banner"></header>
+  <main>
+   
+    <h1>Enhancement 1</h1>
+    <h2>#</h2>
+    <p>#</p>
+
+    <h2>#</h2>
+    <p>#</p>
+
+    <h3>#</h3>
+    <p>#</p>
+     <iframe src="#"></iframe>
+
+    <h2>#</h2>
+    <p>#</p>
+
+    <h2>#</h2>
+    <p>#</p>
+
+    <h2>$</h2>
+    <h3>#</h3>
+    <p>#<br>
+     <img src="#"></p>
+
+    <p>#</p>
+    
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>
+     <li>#</li>     
+
+    <h2>#</h2>
+    <ul>#</ul>
+    <h2>#</h2>
+    <p>#</p>
+    <ul>#</ul>
+   
+  </main>
+ </div>
+</body>
+
+</html>
